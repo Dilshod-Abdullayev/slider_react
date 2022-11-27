3f63a523458e89e1ac32d81dee273f4da991c811
Dilshod Abdullayev <72758099+Dilshod-Abdullayev@users.noreply.github.com>
2022-11-27 17:38:07 +0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
+import React, { useState } from "react";
+import { Data } from "./Data";
+import "./App.css";
+import { Button, Image } from "antd";
+export default function App() {
+  const [count, setCount] = useState(0);
+  let x = Data[count];
+  const Increment = function () {
+    count > 0 ? setCount(count - 1) : setCount((count = 1));
+  };
+  const Decrement = function () {
+    count >= Data.length - 1
+      ? setCount(count - Data.length + 1)
+      : setCount(count + 1);
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="main">
+      <h1>Bizdagi jami rasmlar soni {Data.length}</h1>
+      <div className="card" style={{ display: "flex" }}>
+        <Button onClick={Increment} size="large">❮orqaga</Button>
+        <Image className="fade" width={800} height={500} src={x.url} />
+        <Button onClick={Decrement} type="primary" size="large">
+          Keyingi❯
+        </Button>
+      </div>
     </div>
   );
 }
-
-export default App;
---
+++ src/Data.js
@@ -0,0 +1,42 @@
+export const Data=[
+    {
+        name:'Title',
+        url:'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTUZWqEtuditcxs0HGlk-RPfBb__ytcy63A2xKrWbPN_q7CMdpV_-ZhqmT4W_tf4vrEKRQ&usqp=CAU'
+    },
+    {
+        name:'Title2',
+        url:'https://images.unsplash.com/photo-1598755257130-c2aaca1f061c?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8M3x8d2lsZCUyMGFuaW1hbHxlbnwwfHwwfHw%3D&w=1000&q=80'
+    },
+    {
+        name:'Title2',
+        url:'https://res.cloudinary.com/dk-find-out/image/upload/q_80,c_limit,h_840,w_1240,f_auto/GettyImages_150644751_qknq07.png'
+    },
+    {
+        name:'Title',
+        url:'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQjGZeGigf9vkVR-YreVENETvl2qEyaulGoGQ&usqp=CAU'
+    },
+    {
+        name:'Title2',
+        url:'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTpzpeocx-X-AF0_Hs20J8PlE-_2vZIqM7sUw&usqp=CAU'
+    },{
+        name:'Title2',
+        url:'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRxOw9CCyK6g6ADDlrym0aupn8WWbfrHHe9n_Wdgvl9Ga2hJjiRvYC5z70wbXPCnu-4WHo&usqp=CAU'
+    },
+    {
+        name:'Title2',
+        url:'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQGr6jdbYF-2ucZPMPNq8cDGxsrCpMQl1pDxA&usqp=CAU'
+    },
+    {
+        name:'Title2',
+        url:'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSeM-IvG2byg4sUSXszrwAksoNqecYL_Pn9kA&usqp=CAU'
+    },
+    {
+        name:'Title2',
+        url:'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ2IXV2dVNe0RwlvrmviGrUE61D2o0_wZPUwA&usqp=CAU'
+    },
+    {
+        name:'Title2',
+        url:'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRSusYvItnzbIrW3eMEBNqlzg9cvWEOHm4v1Q&usqp=CAU'
+    }
+]
+export default Data;
